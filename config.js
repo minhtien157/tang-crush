839274c6afeea255fb69bc441c9f45f36c5811a9
minhtien157 <85539391+minhtien157@users.noreply.github.com>
2021-06-08 12:30:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ const CONFIG = {
     introTitle: 'Tiền à',
     introDesc: `Dành cho Tiến tí time nhé <3`,
     btnIntro: '  Rồi zô nè ',
-    title: 'Từ lần đầu gặp cậu vào năm lóp 10 thì tớ đã say Tiền mất rồi',
-    desc: ' Tiền đồng ý cho Tiến được iu Tiền nhé <3 ',
-    btnYes: ' OK Tiền đồng ý ',
+    title: 'Lần đầu gặp Tiền Tiến đã say mê Tiền mất rùi hihi',
+    desc: ' Tiền đồng ý cho Tiến được iu thương Tiền nhé <3 ',
+    btnYes: ' OK Tiền đồng ý !! ',
     btnNo: ' Không, ông bị điên à ',
     question:' Lí do nào mà lại không bấm được cái kia chớ, tức quá đi hà ? ',
-    btnReply: 'Gửi cho Tiến',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Gửi cho Tiến đi nè ',
+    reply: 'Thật ra là Tiền cũng có tí cảm tình với Tiến á hì hì ',
     mess: ' Tiến  biết mà <3 . Yêu Tiền nhiều nhiều 😘😘',
     messDesc: 'Click vào okiiiii lun đi nào <3 ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/minhtien.fb.157' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/minhtien.fb.157' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
